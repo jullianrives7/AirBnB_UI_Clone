1413e7f0f71c9208bf7ed4c6ff27be619d492311
jullianrives7 <114496707+jullianrives7@users.noreply.github.com>
2022-12-13 13:33:57 -0600

    dev test push

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more. MAIN BRANCH.
+        Click on the Vite and React logos to learn more. DEV BRANCH.
       </p>
     </div>
   );
