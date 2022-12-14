310be681b3f28c307eb568aeaa51d02563da1b9f
Lordprettyflacko <snsolomar@gmail.com>
2022-12-14 17:36:45 -0600

    third commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { AiOutlineCheckSquare } from "react-icons/all";
-import NavBar from "./NavBar";
 import Title from "./title";
 import React, { useEffect, useState } from "react";
 import NavBar from "./components/NavBar";
