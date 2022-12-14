7b2ba8d7c9048fe5e9b0b640664589a8183a1251
Lordprettyflacko <snsolomar@gmail.com>
2022-12-14 14:58:17 -0600

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { AiOutlineCheckSquare } from "react-icons/all";
 import NavBar from "./NavBar";
+import Title from "./title";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <AiOutlineCheckSquare />
-      Hello World! (DEV BRANCH)
+      {/* <NavBar />
+      <AiOutlineCheckSquare /> */}
+      <Title />
+
     </div>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./main.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
