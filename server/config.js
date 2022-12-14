9e2d094af6d927a6753f3e80e5f8e3a0f7a8dc68
jullianrives7 <114496707+jullianrives7@users.noreply.github.com>
2022-12-14 17:15:19 -0600

    render server initial config

---
+++ server/config.js
@@ -0,0 +1,12 @@
+const ServerConfig = {
+  dev: {
+    connectionString: "postgresql://postgres:docker@127.0.0.1:5432/mvp_db",
+    port: "3001",
+  },
+  production: {
+    connectionString: process.env.POSTGRES_CONNECTION_STRING + "?ssl=true",
+    port: process.env.PORT,
+  },
+};
+
+export default ServerConfig;
--- server/server.js
+++ server/server.js
@@ -1,113 +1,116 @@
-const express = require('express');
-const { Pool } = require('pg');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+
+// const port = 3000;
+// const { Pool } = require("pg");
+// const pool = new Pool({
+//     user: "postgres",
+//     host: "127.0.0.1",
+//     database: "airbnb",
+//     password: "docker",
+//     port: 5432,
+// });
+const config = require("./config")[process.env.NODE_ENV || "production"];
+const PORT = config.port;
 const app = express();
-const port = 3000;
-const pool = new Pool({
-    user: 'postgres',
-    host: '127.0.0.1',
-    database: 'airbnb',
-    password: 'docker',
-    port: 5432,
-});
+
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/host',(req, res)=>{
-    async function getHost(){
-        try{
-            const result = await pool.query('SELECT * FROM host');
-            res.status(200).send(result.rows);
-        }catch(e) {
-            console.error(e.stack);
-        }
+app.get("/host", (req, res) => {
+  async function getHost() {
+    try {
+      const result = await pool.query("SELECT * FROM host");
+      res.status(200).send(result.rows);
+    } catch (e) {
+      console.error(e.stack);
     }
-    getHost();
+  }
+  getHost();
 });
 
-
-app.get('/host/:id',(req, res)=>{
-    async function getByIdHost(){
-        try{
-            const result = await pool.query(`SELECT * FROM host WHERE host_id = ${req.params.id}`);
-            if(result.rows.length==0){
-                res.sendStatus(404);
-            }else{
-                res.status(200).send(result.rows[0]);
-            }
-        }catch(e) {
-            console.error(e.stack);
-        }
+app.get("/host/:id", (req, res) => {
+  async function getByIdHost() {
+    try {
+      const result = await pool.query(
+        `SELECT * FROM host WHERE host_id = ${req.params.id}`
+      );
+      if (result.rows.length == 0) {
+        res.sendStatus(404);
+      } else {
+        res.status(200).send(result.rows[0]);
+      }
+    } catch (e) {
+      console.error(e.stack);
     }
-    getByIdHost();
+  }
+  getByIdHost();
 });
 
-
-app.get('/rental',(req, res)=>{
-    async function getRental(){
-        try{
-            const result = await pool.query('SELECT * FROM rental');
-            res.status(200).send(result.rows);
-        }catch(e) {
-            console.error(e.stack);
-        }
+app.get("/rental", (req, res) => {
+  async function getRental() {
+    try {
+      const result = await pool.query("SELECT * FROM rental");
+      res.status(200).send(result.rows);
+    } catch (e) {
+      console.error(e.stack);
     }
-    getRental();
+  }
+  getRental();
 });
 
-
-app.get('/rental/:id',(req, res)=>{
-    async function getByIdRental(){
-        try{
-            const result = await pool.query(`SELECT * FROM rental WHERE rental_id = ${req.params.id}`);
-            if(result.rows.length==0){
-                res.sendStatus(404);
-            }else{
-                res.status(200).send(result.rows[0]);
-            }
-        }catch(e) {
-            console.error(e.stack);
-        }
+app.get("/rental/:id", (req, res) => {
+  async function getByIdRental() {
+    try {
+      const result = await pool.query(
+        `SELECT * FROM rental WHERE rental_id = ${req.params.id}`
+      );
+      if (result.rows.length == 0) {
+        res.sendStatus(404);
+      } else {
+        res.status(200).send(result.rows[0]);
+      }
+    } catch (e) {
+      console.error(e.stack);
     }
-    getByIdRental();
+  }
+  getByIdRental();
 });
 
-
-app.get('/review',(req, res)=>{
-    async function getReview(){
-        try{
-            const result = await pool.query('SELECT * FROM review');
-            res.status(200).send(result.rows);
-        }catch(e) {
-            console.error(e.stack);
-        }
+app.get("/review", (req, res) => {
+  async function getReview() {
+    try {
+      const result = await pool.query("SELECT * FROM review");
+      res.status(200).send(result.rows);
+    } catch (e) {
+      console.error(e.stack);
     }
-    getReview();
+  }
+  getReview();
 });
 
-
-app.get('/review/:id',(req, res)=>{
-    async function getByIdReview(){
-        try{
-            const result = await pool.query(`SELECT * FROM review WHERE review_id = ${req.params.id}`);
-            if(result.rows.length==0){
-                res.sendStatus(404);
-            }else{
-                res.status(200).send(result.rows[0]);
-            }
-        }catch(e) {
-            console.error(e.stack);
-        }
+app.get("/review/:id", (req, res) => {
+  async function getByIdReview() {
+    try {
+      const result = await pool.query(
+        `SELECT * FROM review WHERE review_id = ${req.params.id}`
+      );
+      if (result.rows.length == 0) {
+        res.sendStatus(404);
+      } else {
+        res.status(200).send(result.rows[0]);
+      }
+    } catch (e) {
+      console.error(e.stack);
     }
-    getByIdReview();
+  }
+  getByIdReview();
 });
 
-
-
-app.listen(port, () => {
-    console.log(`listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
