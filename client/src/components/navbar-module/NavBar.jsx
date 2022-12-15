b7daf8abcf6c7665a06b9dbb11a80da2f8e40ab1
Fernando castro <fernandocastro77@yahoo.com>
2022-12-15 15:56:51 -0800

    almost done with navbar dec15th

--- client/src/components/navbar-module/NavBar.jsx
+++ client/src/components/navbar-module/NavBar.jsx
@@ -1,52 +1,72 @@
+import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import Offcanvas from "react-bootstrap/Offcanvas";
 import { BsHouseDoorFill } from "react-icons/all";
 import { TbWorld } from "react-icons/all";
 import { BsPerson } from "react-icons/all";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
+import { BiSearchAlt2 } from "react-icons/all";
 
 function NavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-        <BsHouseDoorFill fill="white" stroke="white" />
-        <Navbar.Brand href="#home">Lairbnb</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <InputGroup id="input" className="mb-3">
-              <Form.Control placeholder="Start Your search" />
-              <Button variant="outline-secondary" id="button-addon2">
-                Button
-              </Button>
-            </InputGroup>
-          </Nav>
-          <TbWorld fill="dark" stroke="white" />
-          <Nav>
-            <BsPerson
-              fill="white"
-              stroke="white"
-              id="collasible-nav-dropdown"
-            />
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Sign up</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Log in</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Airbnb your home
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">
-                Host your experience
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">Help</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
+    <Navbar id="display" bg="dark" className="mb-3">
+      <Container fluid>
+        <BsHouseDoorFill id="house-icon" fill="white" stroke="white" />
+        <Navbar.Brand id="brand" href="#home">
+          Lairbnb
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="offcanvasNavbar" />
+        <Navbar.Offcanvas
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+          placement="end"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id="offcanvasNavbarLabel">
+              Offcanvas
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <div id="search">
+                <Button id="button" variant="light">
+                  Start your search
+                  <div id="icon-background">
+                    <BiSearchAlt2
+                      id="search-icon"
+                      fill="white"
+                      stroke="white"
+                    />
+                  </div>
+                </Button>
+              </div>
+              <Nav.Link id="link" href="#action1">
+                Lairbnb your home
+                <TbWorld id="world-icon" fill="dark" stroke="white" />
+              </Nav.Link>
+              <div id="drop-down-bg">
+                <NavDropdown id="offcanvasNavbarDropdown">
+                  <NavDropdown.Item href="#action3">sign up</NavDropdown.Item>
+                  <NavDropdown.Item href="#action3">Log in</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action5">
+                    Lairbnb your home
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action5">
+                    Host your experience
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action5">Help</NavDropdown.Item>
+                </NavDropdown>
+                <div id="person-icon-background">
+                  <BsPerson id="person-icon" fill="white" stroke="white" />
+                </div>
+              </div>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
       </Container>
     </Navbar>
   );
