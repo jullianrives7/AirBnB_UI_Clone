08e9b16196b8c96f1de9a02b49e0263631e8f0d1
jullianrives7 <114496707+jullianrives7@users.noreply.github.com>
2022-12-14 19:06:23 -0600

    render server up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,12 @@ import React, { useEffect, useState } from "react";
 import NavBar from "./components/NavBar";
 
 function App() {
+  const ApiUrl = "https://fec-api-server.onrender.com/";
+  const contextData = {
+    ApiUrl,
+  };
   return (
-    <appContext.Provider value={{}}>
+    <appContext.Provider value={{ ...contextData }}>
       <div>
         <NavBar />
       </div>
