d1df82837c890985ace0ca4f63cc77ef3958c55c
Lordprettyflacko <snsolomar@gmail.com>
2022-12-15 16:58:11 -0600

    last commit

--- client/src/components/title-module/TitleLocation.jsx
+++ client/src/components/title-module/TitleLocation.jsx
@@ -4,7 +4,7 @@ import React, { useContext } from "react";
 
 const TitleLocation = () => {
   const { ApiUrl,setRentalData,rentalData } = useContext(appContext);
-  console.log(rentalData)
+
     return (
       <div className="title_module_TitleLocation"> 
         <BsDot /> <b><u>{rentalData.location}</u></b>
