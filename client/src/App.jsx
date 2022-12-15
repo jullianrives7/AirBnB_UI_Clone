b240e83b29dea6ff4e2769df58276787c06cd6e1
Lordprettyflacko <snsolomar@gmail.com>
2022-12-15 11:29:37 -0600

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import { AiOutlineCheckSquare } from "react-icons/all";
 import NavBar from "./NavBar";
 import Title from "./title";
+import InformationModule from "./information";
 
 function App() {
   return (
     <div className="App">
       {/* <NavBar />
       <AiOutlineCheckSquare /> */}
-      <Title />
+      {/* <Title /> */}
+      <InformationModule />
 
     </div>
   );
