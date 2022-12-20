a4ffbcfbf6bd609119eea77c117926fa61a3337c
HuyNguyen09196 <huynguyen09196@gmail.com>
2022-12-19 18:16:21 -0600

    Huy updated

--- client/src/components/information-module/information-aircover/InformationAirCover.jsx
+++ client/src/components/information-module/information-aircover/InformationAirCover.jsx
@@ -3,20 +3,30 @@ import Button from "react-bootstrap/Button";
 import { useContext } from "react";
 import { appContext } from "../../../App";
 
-
 const InformationAirCover = () => {
-    const { setShowAirCoverModal, ApiUrl,setRentalData,rentalData } = useContext(appContext);
-    const handleClick = () => {
-        setShowAirCoverModal(true);
-      };
+  const { setShowAirCoverModal, ApiUrl, setRentalData, rentalData } =
+    useContext(appContext);
+  const handleClick = () => {
+    setShowAirCoverModal(true);
+  };
   return (
     <div>
-          <div> <img className="airCoverImg" src="https://a0.muscache.com/im/pictures/54e427bb-9cb7-4a81-94cf-78f19156faad.jpg"></img>
-            <p className="airCoverDescription">
-            Every booking includes free protection from Host cancellations, listing inaccuracies, and other issues like trouble checking in.
-            </p>
-            <div className="airCoverButton" onClick={handleClick}><b><u>Learn more</u></b></div>
-          </div>
+      <div>
+        {" "}
+        <img
+          className="airCoverImg"
+          src="https://a0.muscache.com/im/pictures/54e427bb-9cb7-4a81-94cf-78f19156faad.jpg"
+        ></img>
+        <p className="airCoverDescription">
+          Every booking includes free protection from Host cancellations,
+          listing inaccuracies, and other issues like trouble checking in.
+        </p>
+        <div className="airCoverButton" onClick={handleClick}>
+          <b>
+            <u>Learn more</u>
+          </b>
+        </div>
+      </div>
     </div>
   );
 };
